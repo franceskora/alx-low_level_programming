1045b89a3ad0f5ea0200a2ccd44b70d7a4506a3f
franceskora <francesanyakora@gmail.com>
2022-10-14 16:48:10 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	{	
+	{
 		if (i == 'e' || i == 'q')
-		continue;
-	putchar(i);
+			continue;
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
