9e7d0b1f04d81b7e0e4dc96e195720e5e7cbef97
franceskora <francesanyakora@gmail.com>
2022-10-21 07:38:18 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers from 0 to 9
+ * user input's number prints to 9, regardless < 9 or > 9
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	printf("%d");
+	return (0);
+}
+
