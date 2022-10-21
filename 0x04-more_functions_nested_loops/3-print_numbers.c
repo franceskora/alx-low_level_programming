7e5194ebf1e054ba16b8bbb9773f920a48bd17c4
franceskora <francesanyakora@gmail.com>
2022-10-21 08:05:12 -0700

    my commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - a function that prints the numbers from 0 to 9
- * user input's number prints to 9, regardless < 9 or > 9
- * Return: Always 0 (Success)
+ * print_numbers - a function that prints the numbers, from 0 to 9
  */
+
 void print_numbers(void)
 {
-	printf("%d");
-	return (0);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+
+	_putchar('\n');
 }
 
