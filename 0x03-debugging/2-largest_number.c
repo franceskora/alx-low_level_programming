841ca673389c84876719c691ca5472dc3366de36
franceskora <francesanyakora@gmail.com>
2022-10-21 03:05:06 -0700

    my commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
-		printf("Largest = a");
+		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
-		printf("Larest = b");
+		largest = b;
 	}
-	else if (c > a && a > b)
+	else
 	{
-		printf("Largest = c");
+		largest = c;
 	}
-	printf("%d is the largest number\n", largest);
 	
 	return (largest);
 }
