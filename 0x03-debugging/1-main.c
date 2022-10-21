2dd15cdcbdb3821a860165c595e627fcd4ac7205
franceskora <francesanyakora@gmail.com>
2022-10-20 17:45:28 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ int main(void)
 
 	int = 0;
 
-	/*while (i < 10)*/
-	/*{*/
-	/* putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avoided! \\o/\n");
-
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	printf("Infinite loop avoided! \o/\n");
 	return (0);
 }
+
