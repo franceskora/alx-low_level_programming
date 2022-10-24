dcbc2ce7966beed544b4d74209d8a2c85575e3ed
franceskora <francesanyakora@gmail.com>
2022-10-24 11:58:42 -0700

    my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,11 @@ int _strlen(char *s);
 void print_rev(char *s);
 void print_array(int *a, int n);
 int _atoi(char *s);
+int _putchar(char);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+
 
 #endif
