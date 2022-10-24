19b88805a8a5ab1bd26dcd9828ca872974134cd5
franceskora <francesanyakora@gmail.com>
2022-10-24 09:33:20 -0700

    my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 void reset_to_98(int*n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 int _strlen(char *s);
 void print_rev(char *s);
+void print_array(int *a, int n);
 
 #endif
