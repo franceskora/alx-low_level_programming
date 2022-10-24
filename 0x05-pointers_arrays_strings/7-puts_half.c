e16ae8b367c0b09c59de81c57613d945bd99f2e1
franceskora <francesanyakora@gmail.com>
2022-10-24 14:05:34 -0700

    my commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ void puts_half(char *str)
 {
 	int count = 0, i;
 
-	    while (count >= 0)
-	    {
-		    if (str[count] == '\0')
-			    break;
-		    count++;
-	    }
+	while (count >= 0)
+       	{
+	       	if (str[count] == '\0')
+		       	break;
+		count++;
+	}
 
 	if (count % 2 == 1)
 		i = count / 2;
