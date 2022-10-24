21138c8d421ef2078836a8d0190f8933961cf4c0
franceskora <francesanyakora@gmail.com>
2022-10-24 08:42:06 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap-int - swaps the value of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+       *a = *b;
+*a = tmp;
+}
