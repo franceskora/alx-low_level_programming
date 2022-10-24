ab8544ebf3cf5466b9b98381a7063101a80edcca
franceskora <francesanyakora@gmail.com>
2022-10-24 09:45:47 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Description: prints _putchar
+ * Returns: 0 Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar(str[i]);
+
+	return (0);
+}
