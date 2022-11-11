478daa93cd19955357efb999b2fc7c2e092091ca
franceskora <francesanyakora@gmail.com>
2022-11-10 19:20:22 -0800

    my commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; < nmemb * size; i++)
+	for (i = 0; i < nmemb * size; i++)
 		array[i] = 0;
 
 	return (array);
