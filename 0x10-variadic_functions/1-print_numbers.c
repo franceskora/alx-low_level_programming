053c397110473947743613b83a011f8f7cbf2350
franceskora <francesanyakora@gmail.com>
2022-11-17 14:26:40 -0800

    my commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,13 +17,13 @@ void print_numbers(const char *seperator, const unsigned int n, ...)
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
-			{
-			num = va_arg(list, int);
-			printf("%d", num);
+	{
+		num = va_arg(list, int);
+		printf("%d", num);
 
-			if (seperator && i < n - 1)
-				printf("%s", seperator);
-			}
+		if (seperator && i < n - 1)
+			printf("%s", seperator);
+	}
 
 	va_end(list);
 	printf("\n");
