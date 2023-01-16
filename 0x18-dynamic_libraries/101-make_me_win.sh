5218f042c3eeb1d1c29db53cd8f555c76bbffff2
franceskora <francesanyakora@gmail.com>
2023-01-16 14:04:34 -0800

    my commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -fpic *.c
-gcc -shared -o liball.so *.o
+wget -P /tmp https://github.com/franceskora/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
